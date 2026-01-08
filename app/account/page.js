2e72f70554ad87dcc18f6cd355e979e1d2c609df
SHR2000 <shahriarrouhi20001973@gmail.com>
2026-01-08 14:11:12 +0300

    First name capitalization in the account page

--- app/account/page.js
+++ app/account/page.js
@@ -7,10 +7,12 @@ export const metadata = {
 export default async function Page() {
   const session = await auth();
   const firstName = session?.user?.name.split(" ").at(0);
+  const firstNameCapitalized =
+    firstName.charAt(0).toUpperCase() + firstName.slice(1);
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, {firstName}
+      Welcome, {firstNameCapitalized}
     </h2>
   );
 }
